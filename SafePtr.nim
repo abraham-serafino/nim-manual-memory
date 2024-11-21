0f764fa70c7df5e7f8530a9b7ee344467ee136c7
Abraham Serafino <connect@AbrahamSerafino.com>
2024-11-21 09:41:06 -0600

    * Use sizeof for SafePtr type safety * Allow SafePtr's to be explicitly copied

--- SafePtr.nim
+++ SafePtr.nim
@@ -1,33 +1,46 @@
-type SafePtr*[T] = object
-  data: ptr T
+type SafePtr* = object
+  size: 0..high(int)
+  data: pointer
 
-proc `=copy`*[T] (dest: var SafePtr[T], source: SafePtr[T]) =
+proc `=copy`* (dest: var SafePtr, source: SafePtr) =
   assert false,
     "A SafePtr cannot be copied; consider moving it instead."
 
-proc conjure*[T] (t: typedesc[T]): SafePtr[T] =
-  result.data = cast[ptr T](
-    alloc0(sizeof(T)))
+proc copy* (dest: var SafePtr, source: SafePtr) =
+  let size = source.size
+  if dest.data != nil: dealloc(dest.data)
 
+  dest.size = size
+  dest.data = alloc(size)
+  assert dest.data != nil, "Allocation failed."
+
+  dest.data.copyMem(source.data, size)
+
+proc conjure* (t: typedesc): SafePtr =
+  let size = sizeof(t)
+  result.size = size
+  result.data = alloc0(size)
   assert result.data != nil, "Allocation failed."
 
-proc banish*[T] (this: var SafePtr[T]) =
+proc banish* (this: var SafePtr) =
   if this.data != nil:
     dealloc(this.data)
     this.data = nil
 
-proc isNil*[T] (this: SafePtr[T]): bool = this.data == nil
+proc isNil* (this: SafePtr): bool = this.data == nil
 
-proc isNotNil*[T] (this: SafePtr[T]): bool = this.data != nil
+proc isNotNil* (this: SafePtr): bool = this.data != nil
 
-proc `[]`*[T] (this: SafePtr[T]): T =
+proc `[]`*[T] (this: SafePtr, t: typedesc[T]): var T =
   assert this.data != nil, "Nil pointer access."
-  result = this.data[]
+  assert this.size >= sizeof(t),
+    "Pointer references an object larger than the specified type."
 
-proc `[]=`*[T] (this: SafePtr[T], value: T) =
-  assert this.data != nil, "Nil pointer access."
-  this.data[] = value
+  result = cast[ptr T](this.data)[]
 
-template `->`*[T: object] (this: SafePtr[T], prop: untyped): untyped =
+proc `[]=`*[T] (this: var SafePtr, t: typedesc[T], value: T) =
   assert this.data != nil, "Nil pointer access."
-  this.data.prop
+  assert this.size >= sizeof(t),
+    "Pointer references an object larger than the specified type."
+
+  cast[ptr T](this.data)[] = value
--- SafePtr_tests.nim
+++ SafePtr_tests.nim
@@ -1,14 +1,24 @@
 import unittest, SafePtr
 
 suite "SafePtr":
-  test "SafePtr's cannot be copied":
+  test "SafePtr's cannot be implicitly copied":
     expect AssertionDefect:
-      var ptr1: SafePtr[int]
+      var ptr1: SafePtr
       var ptr2 = ptr1
+
+  test "Explicitly copying a SafePtr just copies its data":
+    var ptr1: SafePtr = conjure int
+    ptr1[int] = 3
+
+    var ptr2: SafePtr
+    ptr2.copy(ptr1)
+
+    check ptr2 != ptr1
+    check ptr2[int] == 3
 
   test "conjure() creates a SafePtr":
     let myPtr = conjure int
-    check myPtr of SafePtr[int]
+    check myPtr is SafePtr
 
   test "banish() deallocates and sets the pointer to nil":
     var myPtr = conjure int
@@ -20,26 +30,32 @@ suite "SafePtr":
     banish myPtr
 
     expect AssertionDefect:
-      let temp = myPtr[]
+      let temp = myPtr[array[5, bool]]
 
   test "[]= operator cannot dereference a nil pointer":
     var myPtr = conjure cstring
     banish myPtr
 
     expect AssertionDefect:
-      myPtr[] = cstring("Hello world")
+      myPtr[cstring] = cstring("Hello world")
 
   test "[] and []= operators dereference the pointer":
     var myPtr = conjure int
-    myPtr[] = 5
-    check myPtr[] == 5
+    myPtr[int] = 5
+    check myPtr[int] == 5
 
-  test "-> operator allows object pointers to be mutated":
+  test "[] and []= operators are type safe":
+    var myPtr = conjure int8
+
+    expect AssertionDefect:
+      discard myPtr[int32]
+
+  test "[] operator allows object pointers to be mutated":
     type TestObj = object
       testValue: int
 
     var myPtr = conjure TestObj
-    myPtr->testValue = 5
-    check myPtr[].testValue == 5
+    myPtr[TestObj].testValue = 5
+    check myPtr[TestObj].testValue == 5
 
 echo ""
